42c772ae7d8a5661e0ff7fe88ea06ce9f1350691
strix-src <wrw2981180390@outlook.com>
2026-09-08 20:17:35 +0800

    feat:关于计算时间差

---
+++ time_difference.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int hour1,minute1,hour2,minute2;
+    scanf("%d %d",&hour1,&minute1);
+    scanf("%d %d",&hour2,&minute2);
+
+    int ih = hour1 - hour2 ;
+    int im = minute1 - minute2;
+    if ( im < 0 )
+    {
+        im = im + 60;
+        ih --;
+    }
+    
+    printf("时间差为%d小时%d分钟\n",ih,im);
+    return 0;
+}
