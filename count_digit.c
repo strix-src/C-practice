913b276df7cae50b5924542be3527c01575ced60
strix-src <wrw2981180390@outlook.com>
2026-09-11 14:22:21 +0800

    修改输入0时候的bug，以及将int溢出修改，使之最大识别19位

--- count_digit.c
+++ count_digit.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 int main(void)
 {
-    int n=0,x;
-    scanf("%d",&x);
+    int n=0;
+    long long x;
+
+    scanf("%lld",&x);
     while(x>0){
         n++;
         x/=10;
     }
+    if (n==0) n=1;
     printf("这个数字的位数是%d\n",n);
     return 0;
 }
