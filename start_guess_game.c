2eca55a2af8e1cde359b2d8027eb1da66d753a17
strix-src <wrw2981180390@outlook.com>
2026-09-12 13:55:40 +0800

    一个简陋的猜数小游戏

---
+++ start_guess_game.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+    const int target=123;
+    int guess_num;
+    int count=0;
+
+    printf("请输入一个数字，来猜目标数字\n");
+    do {
+        scanf("%d",&guess_num);
+        count++;
+        if(guess_num < target){
+            printf("再输入一个大一点的数字试试\n");
+        }else if(guess_num > target){
+            printf("再输入一个小一点的数字试试\n");
+        }
+    }while(guess_num != target);
+    printf("恭喜你答对了，你一共尝试了%d次\n",count);
+    return 0;
+}
+        
