80525570c16cb736d634e1d8ed35e06a6198160b
strix-src <wrw2981180390@outlook.com>
2026-09-14 13:09:57 +0800

    feat:判断是否为素数

---
+++ is_prime.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    long long a;
+    long long i;
+    int isPrime=1;
+    printf(" 请随机输入一个数，我将判断是否为素数\n");
+    scanf("%lld",&a);
+    for (i=2;i<a;i++){
+        if(a%i == 0){
+            isPrime=0;
+        }
+    }
+    if(isPrime == 0){
+        printf("这不是一个素数\n");
+    }else{
+        printf("这是一个素数\n");
+    }
+    return 0;
+}
+
